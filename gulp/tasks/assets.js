5b806ff48f541c215ea603e414abb4796898f6f0
Barandis <barandis1729@gmail.com>
2016-03-20 06:40:29 -0400

    Added topic-specific asset task

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -3,7 +3,34 @@
 const gulp   = require('gulp');
 const config = require('../config');
 
-gulp.task('assets', () => {
+gulp.task('assets:common', () => {
   return gulp.src(`${config.src.assets}/**/*`)
     .pipe(gulp.dest(config.build.assets));
+});
+
+gulp.task('assets:topic', () => {
+  const basePath = config.base;
+  const topicPath = `${config.base}/${config.topic}`;
+  const src = config.topic === '' ? 
+    [ 
+      `${basePath}/**/*`,
+      `!${basePath}/**/*.js`,
+      `!${basePath}/**/*.ls`,
+      `!${basePath}/**/*.css`,
+      `!${basePath}/**/*.styl`,
+      `!${basePath}/**/*.html`,
+      `!${basePath}/**/*.jade` 
+    ] : 
+    [ 
+      `${topicPath}/**/*`,
+      `!${topicPath}/**/*.js`,
+      `!${topicPath}/**/*.ls`,
+      `!${topicPath}/**/*.css`,
+      `!${topicPath}/**/*.styl`,
+      `!${topicPath}/**/*.html`,
+      `!${topicPath}/**/*.jade`
+    ];
+  const dest = config.topic === '' ? config.build.root : `${config.build.root}/${config.topic}`;
+  return gulp.src(src)
+    .pipe(gulp.dest(dest));
 });
